dc43738d627f97c10712f655159d03ed24bb694b
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2017-02-27 18:26:00 +0100

    add brew formula

---
+++ testscript.rb
@@ -0,0 +1,13 @@
+
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/gborowik/testscript/archive/v1.0.0.tar.gz"
+  sha256 "a2cd0df31388b97b9474ffd527e4ae280e9c6da2dafcc1d6f1b0cbdbff119c81"
+
+
+  def install
+	bin.install "testscript"
+  end
+
+end
