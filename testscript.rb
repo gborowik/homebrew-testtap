756a2aac1546bf8680a5ff6acfd56ea818d64cf6
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2017-02-27 18:42:32 +0100

    dependency test

--- testscript.rb
+++ testscript.rb
@@ -5,6 +5,7 @@ class Testscript < Formula
   url "https://github.com/gborowik/testscript/archive/v1.0.0.tar.gz"
   sha256 "a2cd0df31388b97b9474ffd527e4ae280e9c6da2dafcc1d6f1b0cbdbff119c81"
 
+  depends_on "sip"
 
   def install
 	bin.install "testscript"
