544f8d00e3261e0804667bcca093bd508c7b0b45
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2017-02-27 19:29:34 +0100

    dependency test3

--- testscript.rb
+++ testscript.rb
@@ -5,13 +5,11 @@ class Testscript < Formula
   url "https://github.com/gborowik/testscript/archive/v1.0.0.tar.gz"
   sha256 "a2cd0df31388b97b9474ffd527e4ae280e9c6da2dafcc1d6f1b0cbdbff119c81"
 
-  resource "sip" do
-    url "https://github.com/golemfactory/golem/wiki/wheels/sip-4.19-cp27-cp27m-macosx_10_12_x86_64.whl"
-    sha256 "65e8670bf1f72c54a1cdc0a4756c42951530f9bce7bac7ac5d2df99a727f37c4"
-  end
-
   def install
 	bin.install "testscript"
+
+  resource("https://github.com/golemfactory/golem/wiki/wheels/sip-4.19-cp27-cp27m-macosx_10_12_x86_64.whl")
+      .stage { system python, "setup.py", "install" }
   end
 
 end
